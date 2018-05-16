1b5e5664ad2f384e8cc4f0385852adb4328fbecd
Erich Schroeter <erich.schroeter@magnetek.com>
2018-05-16 10:07:23 -0500

    Implemented main to use Blinker_Task

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,26 @@
 #include <iostream>
+// #include <unistd.h>
+#include <chrono>
+#include <thread>
+
+#include <MyCompany/tasks/Blinker_Task.h>
+
+using namespace MyCompany;
 
 int main(int argc, char **argv)
 {
-    std::cout << "Hello World" << std::endl;
+    Blinker_Task task;
+    int loop_time_in_ms = 10;
+    // int loop_time_in_us = 10 * 1000;
+    task.Set_Loop_Time( loop_time_in_ms );
+
+    do
+    {
+        task.Run();
+        // usleep( loop_time_in_us );
+        std::this_thread::sleep_for( std::chrono::milliseconds( loop_time_in_ms ) );
+    } while ( true );
+
     return 0;
 }
 
